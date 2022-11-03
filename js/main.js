95cdebcda375a5098dd974089af88d0082a63300
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-03 20:29:36 +0100

    Aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
 `use strict`
 const btnGenerator = document.getElementById("btn-generator");
 const boardContainer = document.querySelector(".board");
+let pointsDown = document.getElementById("points");
 let bombs = [];
 let numberCell = 0;
-let pointsDown = document.getElementById("points");
 
-    function getRndInteger(min, numCell) {
+btnGenerator.addEventListener('click', function(){
+    const level = document.getElementById("difficolta").value;
+    if(level === "hard"){
+        numberCell = 100;
+    }else if(level === "medium"){
+            numberCell = 81;
+    }else if(level === "easy"){
+            numberCell = 49;
+    } 
+    console.log(numberCell, level);
+    getRndInteger(1, numberCell);
+    console.log(bombs, 'Numero CPU');
+    tab(numberCell, boardContainer);   
+})
+    /* [1, 2, 3, 4].includes(2) */
+
+    function getRndInteger(min, max) {
         bombs = [];
         let bombNumber = 0;
         while(bombs.length < 16){
-         bombNumber = Math.floor(Math.random() * (numCell - min + 1) ) + min; 
-         if(!(bombs.includes(bombNumber))){
+        /* numero da 1 al max */
+         bombNumber = Math.floor(Math.random() * (max - min + 1) ) + min; 
+         if(!bombs.includes(bombNumber)){
             bombs.push(bombNumber);
          }           
         }         
     }
 
     function tab(numCell, element){
+        // numCell = numberCell
+        // element = boardContainer
         element.innerHTML = "";
         let points = 0;
-        pointsDown.innerHTML = `Questo è il tuo punteggio: ${points}`;
+        pointsDown.innerHTML = `Questo è il tuo punteggio: ${points}`; // 'Questo è il tuo punteggio: ' + points;
         for( let i = 1; i <= numCell; i++){
             const boardCell = document.createElement("div");
             boardCell.innerHTML = i; 
@@ -32,7 +51,8 @@ let pointsDown = document.getElementById("points");
             }
             boardCell.addEventListener("click", function(){
                 console.log(this.innerHTML, 'Numero giocatore');
-                if(!(bombs.includes(Number(this.innerHTML)))){
+                let isBomb = bombs.includes(Number(this.innerHTML))
+                if(!isBomb){
                     points = points + 1;
                     boardCell.classList.add("lightblue");
                     pointsDown.innerHTML = `Questo è il tuo punteggio: ${points}`;
@@ -52,17 +72,5 @@ let pointsDown = document.getElementById("points");
     
 
 
-btnGenerator.addEventListener('click', function(){
-    const level = document.getElementById("difficolta").value;
-    if(level === "hard"){
-        numberCell = 100;
-    }else if(level === "medium"){
-            numberCell = 81;
-    }else if(level === "easy"){
-            numberCell = 49;
-    } 
-    getRndInteger(1, numberCell);
-    console.log(bombs, 'Numero CPU');
-    tab(numberCell, boardContainer);   
-})
+
 
