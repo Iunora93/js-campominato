f20f4cde326b8de30ffcfcbaae1d2c521a30edd8
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-03 15:23:34 +0100

    math random nel livello hard

--- js/main.js
+++ js/main.js
@@ -5,12 +5,6 @@ const boardContainer = document.querySelector(".board");
 
 btnGenerator.addEventListener('click', function(){
     const level = document.getElementById("difficolta").value;
-    
-    
-    
-    /* Genero un numero rabdom della cpu da 1 a 16 */
-   
-
     if(level === "hard"){
         boardContainer.innerHTML = "";
         for( let i = 1; i <= 100; i++){
@@ -20,14 +14,7 @@ btnGenerator.addEventListener('click', function(){
             console.log(cpuNumber, 'Numero CPU');
             function getRndInteger(min, max) {
                 return Math.floor(Math.random() * (max - min + 1) ) + min; 
-            }
-            if( boardCell === cpuNumber ) {
-                boardCell.addEventListener("click", function(){
-                    console.log(this.innerHTML, 'Numero giocatore');
-                    boardCell.classList.add("red");
-                })
-                boardContainer.append(boardCell);
-                console.log("Ha vinto la cpu");
+                
             }
             boardCell.classList.add("board__number");
             boardCell.addEventListener("click", function(){
